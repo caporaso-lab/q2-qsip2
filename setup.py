0287ea654fef6c77bc5f54b538167076d26ee937
Colin Wood <colinwood@Colins-MacBook-Air.local>
2024-08-12 11:46:55 -0700

    initial commit
    Created from https://github.com/caporaso-lab/cookiecutter-qiime2-plugin. See https://develop.qiime2.org to learn more.

---
+++ setup.py
@@ -0,0 +1,36 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Colin Wood.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+description = ("A template QIIME 2 plugin.")
+
+setup(
+    name="q2-qsip2",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    author="Colin Wood",
+    author_email="colin.wood@nau.edu",
+    description=description,
+    url="www.qiime2.org",
+    entry_points={
+        "qiime2.plugins": [
+            "q2_qsip2="
+            "q2_qsip2"
+            ".plugin_setup:plugin"]
+    },
+    package_data={
+        "q2_qsip2": ["citations.bib"],
+        "q2_qsip2.tests": ["data/*"],
+    },
+    zip_safe=False,
+)
