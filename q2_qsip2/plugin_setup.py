8c28c9e73aa941ab059ba7f4396074b4173042cd
Colin Wood <colin.wood@nau.edu>
2024-08-13 11:59:27 -0700

    first input wrangling methods and tests

--- q2_qsip2/plugin_setup.py
+++ q2_qsip2/plugin_setup.py
@@ -9,7 +9,6 @@
 from qiime2.plugin import Citations, Plugin
 from q2_types.feature_table import FeatureTable, Frequency
 from q2_qsip2 import __version__
-from q2_qsip2._methods import duplicate_table
 
 citations = Citations.load("citations.bib", package="q2_qsip2")
 
@@ -20,22 +19,5 @@ plugin = Plugin(
     package="q2_qsip2",
     description="A plugin for analyzing quantitative stable isotope probing (qSIP) data.",
     short_description="Analyze qSIP data.",
-    # Please retain the plugin-level citation of 'Caporaso-Bolyen-2024'
-    # as attribution of the use of this template, in addition to any citations
-    # you add.
     citations=[citations['Caporaso-Bolyen-2024']]
-)
-
-plugin.methods.register_function(
-    function=duplicate_table,
-    inputs={'table': FeatureTable[Frequency]},
-    parameters={},
-    outputs=[('new_table', FeatureTable[Frequency])],
-    input_descriptions={'table': 'The feature table to be duplicated.'},
-    parameter_descriptions={},
-    output_descriptions={'new_table': 'The duplicated feature table.'},
-    name='Duplicate table',
-    description=("Create a copy of a feature table with a new uuid. "
-                 "This is for demonstration purposes only. 🧐"),
-    citations=[]
 )
---
+++ q2_qsip2/tests/test_wrangling.py
@@ -0,0 +1,113 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Colin Wood.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+import pandas as pd
+
+import qiime2
+from qiime2.plugin.testing import TestPluginBase
+
+from q2_qsip2._wrangling import (
+    _extract_source_metadata, _validate_metadata_columns
+)
+
+
+class WranglingTests(TestPluginBase):
+    package = 'q2_qsip2.tests'
+
+    def sample_metadata(self):
+        df = pd.DataFrame({
+            'sample-id': ['a', 'b', 'c', 'd'],
+            'source-id': ['s1', 's2', 's1', 's2'],
+            'sample-level-data': ['w', 'x', 'y', 'z'],
+            'source-level-data': ['x', 'y', 'x', 'y'],
+        })
+        df.set_index('sample-id', inplace=True)
+
+        return qiime2.Metadata(df)
+
+    def test_extract_source_metadata(self):
+        extracted = _extract_source_metadata(
+            self.sample_metadata(), 'source-id'
+        )
+
+        # only rows that have a unique 'source-id' are retained; only
+        # source-level columns are retained
+        exp = pd.DataFrame({
+            'source-id': ['s1', 's2'],
+            'source-level-data': ['x', 'y'],
+        })
+
+        self.assertTrue( exp.equals(extracted) )
+
+    def metadata_to_validate(self):
+        df = pd.DataFrame({
+            'sample-id': ['a', 'b', 'c', 'd'],
+            'isotope': ['12C', '12C', '13C', '13C'],
+            'my-isotopolog-col': ['glucose', 'glucose', 'glucose', 'glucose'],
+        })
+        df.set_index('sample-id', inplace=True)
+
+        return qiime2.Metadata(df)
+
+    def metadata_column_mapping(self):
+        return {
+            'isotope': None,
+            'isotopolog': 'my-isotopolog-col',
+        }
+    def test_validate_metadata_columns(self):
+        metadata = self.metadata_to_validate()
+        columns_mapping = self.metadata_column_mapping()
+
+        _validate_metadata_columns(metadata, columns_mapping, 'source')
+
+    def metadata_column_mapping_missing_default(self):
+        return {
+            'isotope': None,
+            'isotopolog': 'my-isotopolog-col',
+            'amount': None,
+        }
+
+    def test_validate_metadata_columns_missing_default(self):
+        metadata = self.metadata_to_validate()
+        columns_mapping = self.metadata_column_mapping_missing_default()
+
+        exp_error = (
+           'The following required columns were not found in the '
+           'source metadata:\n\n'
+           f'{"Default":^25} | {"Provided":^25}\n'
+           f'{"-" * 23:^25} | {"-" * 23:^25}'
+           f'\n{"amount":^25} | {"N/A (default used)":^25}'
+           '\n\nPlease update the column names passed to the method '
+           'or update your metadata to use the defaults.'
+        )
+
+        with self.assertRaisesRegex(ValueError, exp_error):
+            _validate_metadata_columns(metadata, columns_mapping, 'source')
+
+    def metadata_column_mapping_missing_provided(self):
+        return {
+            'isotope': None,
+            'isotopolog': 'my-isotopolog-col',
+            'amount': 'my-amount-col',
+        }
+
+    def test_validate_metadata_columns_missing_provided(self):
+        metadata = self.metadata_to_validate()
+        columns_mapping = self.metadata_column_mapping_missing_provided()
+
+        exp_error = (
+           'The following required columns were not found in the '
+           'source metadata:\n\n'
+           f'{"Default":^25} | {"Provided":^25}\n'
+           f'{"-" * 23:^25} | {"-" * 23:^25}'
+           f'\n{"amount":^25} | {"my-amount-col":^25}'
+           '\n\nPlease update the column names passed to the method '
+           'or update your metadata to use the defaults.'
+        )
+
+        with self.assertRaisesRegex(ValueError, exp_error):
+            _validate_metadata_columns(metadata, columns_mapping, 'source')
